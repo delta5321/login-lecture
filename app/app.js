dfeb17c5284a4de8b18f604b95334627694ee94b
delta5321 <delta5321@naver.com>
2021-07-01 02:52:21 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ app.set("views", "./src/views");    //뷰 폴더 지정 render 사용시 뷰 경
 app.set("view engine", "ejs");  //view engine을 ejs로 사용
 
 
+app.use(express.static(`${__dirname}/src/public`)); //${__dirname}은 현재 디렉토리를 나타냄, 정적 경로 추가
 app.use("/", home); //use - > 미들웨어를 등록하는 코드, 
 
 module.exports = app; // app이라는 이름으로 내보내기
