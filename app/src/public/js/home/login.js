3c9a5eed2cf119d0c73d1724e422ec12c936ac76
delta5321 <delta5321@naver.com>
2021-07-01 10:45:25 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+//선택자를 통해 데이터를 받아오기
+const id = document.querySelector("#id"),
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+
+}
