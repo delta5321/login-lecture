ce674c137436a9d0361198f74514a0bc69cf39d7
delta5321 <delta5321@naver.com>
2021-07-02 11:27:38 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -4,19 +4,24 @@
 
 //선택자를 통해 데이터를 받아오기
 const id = document.querySelector("#id"),
-psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("#button");
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login(){
+function register(){
     const req = {
         id : id.value,
+        name : name.value,
         psword : psword.value,
+        confirmPsword : confirmPsword.value,
     };
+    console.log(req)
 
 // 브라우저에 입력한 아이디와 패스워드를 서버로 전송
-    fetch("/login", {
+    fetch("/register", {
         method : "POST",
         headers : {
             "Content-type" : "application/json" // JSON 형태의 파일이라는 것을 명시적으로 알려줌
@@ -26,13 +31,12 @@ function login(){
     .then((res) => res.json())  // json()으로 Response의 스트림을 완전한 형태로 읽는것( Promise 형태)
     .then((res) => {
         if (res.success) {
-            location.href = "/";    // /로 이동
-            alert(res.id)
+            location.href = "/login";    // /로 이동
         } else {
             alert(res.msg);
         }
     }) //Promise 내부를 까보는것
     .catch((err) => {
-        console.error(new Error("로그인 중 에러 발생"));
+        console.error("회원가입 중 에러 발생");
     });
 }
