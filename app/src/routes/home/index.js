533c3c67a62d205e8502ba8b3c45fb130e76ca4b
delta5321 <delta5321@naver.com>
2021-07-02 11:13:11 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post('/login', ctrl.process.login);
 
 //변수를 외부에서 사용할 수 있도록 해주는 코드(내보내기)
